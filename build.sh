5519f4db3fca07fc9291e29060ebbe8f475cadc8
Wed0n <wed0n1213@gmail.com>
2024-03-29 12:15:01 +0000

    use cmake

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-make server
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE\
+ -DCMAKE_C_COMPILER=clang\
+ -DCMAKE_CXX_COMPILER=clang++\
+ -Bbuild
+cd build;make
+
+# rm -rf CMakeFiles/ cmake_install.cmake CMakeCache.txt Makefile
